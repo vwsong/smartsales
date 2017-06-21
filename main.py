190543bc09e89a177d47d8f7d2a9d09a97819d51
Song, Vincent <Vincent.Song@nordstrom.com>
2017-06-21 11:10:14 -0700

    set up getsale endpoint

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import helper
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/api/getSale/", methods=['POST'])
+    def getSale():
+        ageMin = request.form["ageMin"]
+        ageMax = request.form["ageMax"]
+        gender = request.form["gender"]
+        ethnicity = request.form["ethnicity"]
+        efficiency = request.form["efficiency"]
+        coverage = request.form["coverage"]
+        discount = request.form["discount"]
+
+        if efficiency:
+            return getEfficientSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
+        else:
+            return getRegSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
