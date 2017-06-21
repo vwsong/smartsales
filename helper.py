5720e50d563d67867b82416ab51676922b1da25e
Song, Vincent <Vincent.Song@nordstrom.com>
2017-06-21 11:21:40 -0700

    fixes

--- helper.py
+++ helper.py
@@ -1,1 +1,5 @@
+def sayHello():
+   return 'hello world'
 
+if __name__=='__main__':
+   print function()
--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import helper
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route("/api/getSale/", methods=['POST'])
-    def getSale():
-        ageMin = request.form["ageMin"]
-        ageMax = request.form["ageMax"]
-        gender = request.form["gender"]
-        ethnicity = request.form["ethnicity"]
-        efficiency = request.form["efficiency"]
-        coverage = request.form["coverage"]
-        discount = request.form["discount"]
-
-        if efficiency:
-            return getEfficientSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
-        else:
-            return getRegSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
+def getSale():
+    ageMin = request.form["ageMin"]
+    ageMax = request.form["ageMax"]
+    gender = request.form["gender"]
+    ethnicity = request.form["ethnicity"]
+    efficiency = request.form["efficiency"]
+    coverage = request.form["coverage"]
+    discount = request.form["discount"]
+    return 'Hello'
+        # if efficiency:
+        #     return getEfficientSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
+        # else:
+        #     return getRegSale(ageMin, ageMax, gender, ethnicity, coverage, discount)
