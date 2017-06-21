42f5d5a7ac1a4540c6315e45521f1447cd6b673b
Song, Vincent <Vincent.Song@nordstrom.com>
2017-06-21 15:07:20 -0700

    cleaned up code

--- algo.py
+++ algo.py
@@ -6,34 +6,42 @@ import json
 def isAge(given, ageMin, ageMax):
     return given < ageMax & given > ageMin
 
-def union(arr1, arr2):
-
 def targetDemographic(cData, iData, ageMin, ageMax, gender, ethnicity, coverage, zipcode):
     customerList = []
     itemList = []
     totalCost = 0
 
-    totalDemographic = {k: v for k, v in dict1.items() if v["age"] > ageMin}
-    totalDemographic = {k: v for k, v in dict1.items() if v["age"] > ageMax}
+    # Get total number of people in specified demographic
+    totalDemographic = {k: v for k, v in cData.items() if v["age"] > ageMin}
+    totalDemographic = {k: v for k, v in cData.items() if v["age"] < ageMax}
     if gender != -1:
-        totalDemographic = {k: v for k, v in dict1.items() if v["gender"] == gender}
+        totalDemographic = {k: v for k, v in cData.items() if v["gender"] == gender}
     if ethnicity != -1:
-        totalDemographic = {k: v for k, v in dict1.items() if v["ethnicity"] == ethnicity}
+        totalDemographic = {k: v for k, v in cData.items() if v["ethnicity"] == ethnicity}
     if zipcode != -1:
-        totalDemographic = {k: v for k, v in dict1.items() if v["address"]["zipcode"] == ethnicity}
+        totalDemographic = {k: v for k, v in cData.items() if v["address"]["zipcode"] == ethnicity}
+
+    return totalDemographic #a subset of cData
+
+def getRegSale(ageMin, ageMax, gender, ethnicity, coverage, zipcode, discount):
+    cData = getCustDataFromRedis()
+    iData = getItemDataFromRedis()
+    customerList = []
+    itemList = []
+    totalCost = 0
+    totalDemographic = targetDemographic(cData, iData, ageMin, ageMax, gender, ethnicity, coverage, zipcode)
 
     for key in iData.keys():
         interestedPersons = iData[key]["interestedPersons"]
 
         #Begin filtering!
-        interestedPersons = [person if cData[person]["age"] > ageMin]
-        interestedPersons = [person if cData[person]["age"] > ageMax]
+        interestedPersons = [person for person in interestedPersons if cData[person]["age"] > ageMin and cData[person]["age"] < ageMax]
         if gender != -1:
-            interestedPersons = [person if cData[person]["gender"] == gender]
+            interestedPersons = [person for person in interestedPersons if cData[person]["gender"] == gender]
         if ethnicity != -1:
-            interestedPersons = [person if cData[person]["ethnicity"] == ethnicity]
+            interestedPersons = [person for person in interestedPersons if cData[person]["ethnicity"] == ethnicity]
         if zipcode != -1:
-            interestedPersons = [person if cData[person]["zipcode"] == zipcode]
+            interestedPersons = [person for person in interestedPersons if cData[person]["zipcode"] == zipcode]
 
         # If interestedPersons is not a subset of customerList
         # (if they add new covered customers)
@@ -45,18 +53,10 @@ def targetDemographic(cData, iData, ageMin, ageMax, gender, ethnicity, coverage,
             if currCoverage > coverage:
                 break
 
-    return { "customerList" : customerList, "itemList" : itemList, "totalCost" : totalCost, "coverage" : currCoverage }
+    return { "customerList" : customerList, "itemList" : itemList, "totalCost" : totalCost*discount, "coverage" : currCoverage }
 
 def getEfficientSale(ageMin, ageMax, gender, ethnicity, coverage, zipcode, discount):
     return "getEffSale() not implemented"
 
 def getMinimalSale(ageMin, ageMax, gender, ethnicity, coverage, zipcode, discount):
-
-def getRegSale(ageMin, ageMax, gender, ethnicity, coverage, zipcode, discount):
-    cData = getCustDataFromRedis()
-    iData = getItemDataFromRedis()
-
-    processed = targetDemographic(cData, iData, ageMin, ageMax, gender, ethnicity, coverage, zipcode)
-
-    processed["totalCost"] = processed["totalCost"] * discount
-    return jsonify(processed)
+    return "getMinimalSale() not implemented"
